78f30b17c8f67f52d1b97bf7cfa2774a50a5426b
Kelly-Huynh <96kellyhuynh@gmail.com>
2026-07-15 15:39:13 +0100

    Initial setup of recipe page

---
+++ app/src/main/java/com/example/girldinner/screens/Recipe.kt
@@ -0,0 +1,25 @@
+package com.example.girldinner.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.unit.*
+
+@Composable
+fun Recipe() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White), contentAlignment = Alignment.Center
+    ) {
+        Column {
+            Text(
+                text =
+            )
+        }
+    }
+}
